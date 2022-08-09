a9016ad4d655e927adbcf5896548544da6216905
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-08-09 11:59:58 -0400

    creates vue notes app

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: ["note", "selectedNoteId"],
+  methods: {
+    selectNote: function (note) {
+      this.$emit("selectNote", note);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="note-selector" v-bind:class="{ active: selectedNoteId === note.id }" v-on:click="selectNote(note)">
+    <p class="note-selector-title">{{ note.body }}</p>
+    <p class="note-selector-timestamp">{{ note.timestamp }}.</p>
+  </div>
+</template>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,39 @@
+<script>
+import NoteSelector from "./NoteSelector.vue";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() },
+        { id: 5, body: "This is a fifth test", timestamp: Date.now() },
+        { id: 6, body: "This is a more test", timestamp: Date.now() },
+      ],
+      selectedNoteId: 1,
+    };
+  },
+  methods: {
+    selectNote: function (note) {
+      this.selectedNoteId = note.id;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="note-selectors">
+    <NoteSelector
+      v-for="note in notes"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      v-on:selectNote="selectNote"
+      v-bind:key="note.id"
+    />
+  </div>
+</template>
